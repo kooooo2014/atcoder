0ca214d31a118a9946c6bd918de7530a07edaf06
kooooo2014 <20141763kooooo@gmail.com>
2021-10-21 18:10:28 +0900

    2021/10/21

---
+++ ABC220C - Long Sequence.js
@@ -0,0 +1,35 @@
+function Main(input) {
+    input = input.split("\n");
+    var a = input[1].split(" ");
+    var aSum = BigInt(0);
+    var tmp = BigInt(0);
+
+    
+    for (var i = 0; i < a.length; i++){
+        a[i] = BigInt(parseInt(a[i], 10));
+        aSum += a[i];
+    }
+  
+    var x = BigInt(input[2], 10);
+    var syou = BigInt(Math.floor(Number(x) / Number(aSum)));
+    var count = BigInt(syou  * BigInt(i));
+    var sum = BigInt(syou * aSum);
+
+    for(i = 0; i< a.length ; i++){
+        if(sum > x){
+            break;
+        }
+      
+        sum += a[i];
+        count ++;
+
+        if(i === a.length - 1){
+            i = 0;
+        }
+        
+    }
+    
+    console.log(String(count));  
+}
+
+Main(require("fs").readFileSync("/dev/stdin", "utf8"));
